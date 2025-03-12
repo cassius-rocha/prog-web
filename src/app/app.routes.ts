f2e173032c5427612f82aa27cc8b259f4a9a1831
lsitec-cassiusrocha <cassius.santos@lsitec.org.br>
2025-03-12 13:18:07 -0300

    Cria componentes da atividade web03

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { FaleConoscoComponent } from './components/fale-conosco/fale-conosco.component';
+import { VitrineComponent } from './components/vitrine/vitrine.component';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
+import { LoginComponent } from './components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"faleconosco", component:FaleConoscoComponent},
+    {path:"contato", component:FaleConoscoComponent},
+    {path:"vitrine",component:VitrineComponent},
+    {path:"", component:VitrineComponent},
+    {path:"cadastro", component:CadastroComponent},
+    {path:"login", component:LoginComponent}
+];
